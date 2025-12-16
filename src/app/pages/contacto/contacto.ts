cc85d97edd95ef4a61e18b50d68cca3fbfdb73d0
MARIO <#MARIOPC.INGENIERO@GMAIL.COM>
2025-12-16 00:35:17 -0300

    Fix templateUrl for Quienes Somos and Contacto

--- src/app/pages/contacto/contacto.ts
+++ src/app/pages/contacto/contacto.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  templateUrl: './contacto.component.html',
+  templateUrl: './contacto.html',
   styleUrls: ['./contacto.css']
 })
 export class ContactoComponent {}
--- src/app/pages/quienes-somos/quienes-somos.ts
+++ src/app/pages/quienes-somos/quienes-somos.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-quienes-somos',
   standalone: true,
-  templateUrl: './quienes-somos.component.html',
+  templateUrl: './quienes-somos.html',
   styleUrls: ['./quienes-somos.css']
 })
 export class QuienesSomosComponent {}
